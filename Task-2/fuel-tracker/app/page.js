3f167832397c1cef80edeabcb39f14720b2b35c5
ume <chi.nguyenthiyen.203@gmail.com>
2024-10-09 17:43:29 +0700

    complete Task 2

--- Task-2/fuel-tracker/app/page.js
+++ Task-2/fuel-tracker/app/page.js
@@ -1,5 +1,14 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
-    <></>
+    <div className="flex flex-col items-center justify-center h-screen w-full">
+      <h1 className="mb-4 text-3xl font-bold text-[#1e2a5e]">Welcome to Fuel Tracker</h1>
+      <Link href="/tracker">
+        <button className="text-sm bg-[#1e2a5e] h-fit text-[#edf1fb] hover:bg-[#2e3e86] rounded-lg px-4 py-2 whitespace-nowrap">
+          Go to Tracker Form
+        </button>
+      </Link>
+    </div>
   );
 }
--- Task-2/fuel-tracker/app/tracker/page.js
+++ Task-2/fuel-tracker/app/tracker/page.js
@@ -1,10 +1,10 @@
 "use client";
+import Link from 'next/link';
 
 import React, { useState } from "react";
 import { FaArrowLeft } from "react-icons/fa";
 
 const page = () => {
-
   const [error, setError] = useState(null);
   const [time, setTime] = useState("");
   const [quantity, setQuantity] = useState("");
@@ -20,78 +20,144 @@ const page = () => {
       return;
     }
 
+    // Check for valid time
+    const inputtedTime = new Date(time);
+    const currentTime = new Date();
+    if (inputtedTime > currentTime) {
+      setError("Thời gian được nhập không được lớn hơn thời gian hiện tại");
+      return;
+    }
+    if (isNaN(inputtedTime.getTime())) {
+      setError("Thời gian được nhập không hợp lệ.");
+      return;
+    }
+
+    if (!quantity) {
+      console.log(time);
+      setError("Hãy nhập Số lượng");
+      return;
+    }
+
+    if (!pump) {
+      setError("Hãy chọn Trụ");
+      return;
+    }
+
+    if (!revenue) {
+      setError("Hãy nhập Doanh thu");
+      return;
+    }
+
+    if (!price) {
+      setError("Hãy nhập Đơn giá");
+      return;
+    }
+
     setError(null);
+    alert("Cập nhật thành công");
+    setTime("");
+    setQuantity("");
+    setPump("");
+    setRevenue("");
+    setPrice("");
   };
 
   return (
-    <div className="flex items-center justify-center h-screen w-full">
+    <div className="flex items-center justify-center h-screen w-full relative">
       <form
-        onSubmit={handleSubmit}
-        className="w-[50%] h-fit bg-[#fff] rounded-lg text-[#1e2a5e]"
-        style={{ boxShadow: "0 2px 3px rgba(30,42,94, 0.1)" }}
-      >
-        <header
-          className="p-6 flex"
-          style={{ boxShadow: "0 4px 6px rgba(30,42,94, 0.1)" }}
+          onSubmit={handleSubmit}
+          className="w-[50%] h-fit bg-[#fff] rounded-lg text-[#1e2a5e]"
+          style={{ boxShadow: "0 2px 3px rgba(30,42,94, 0.1)" }}
         >
-          <div className="justify-between w-full text-start">
-            <button className="text-sm flex items-center cursor-pointer hover:underline">
-              <FaArrowLeft />
-              Đóng
-            </button>
-            <h1 className="font-bold text-2xl pt-4">Nhập giao dịch</h1>
-          </div>
-          <button
-            className="text-sm bg-[#1e2a5e] h-fit text-[#edf1fb] hover:bg-[#2e3e86] rounded-lg px-4 py-2 whitespace-nowrap"
-            type="submit"
+          <header
+            className="p-6 flex"
+            style={{ boxShadow: "0 4px 6px rgba(30,42,94, 0.1)" }}
           >
-            Cập nhật
-          </button>
-        </header>
+            <div className="justify-between w-full text-start">
+              <Link href="/">
+                <button
+                  className="text-sm flex items-center cursor-pointer hover:underline"
+                >
+                  <FaArrowLeft />
+                  Đóng
+                </button>
+                </Link>
+              <h1 className="font-bold text-2xl pt-4">Nhập giao dịch</h1>
+            </div>
+            <button
+              className="text-sm bg-[#1e2a5e] h-fit text-[#edf1fb] hover:bg-[#2e3e86] rounded-lg px-4 py-2 whitespace-nowrap"
+              type="submit"
+            >
+              Cập nhật
+            </button>
+          </header>
 
-        <div className="p-4">
-          {error && (
-            <p className="text-red-500 text-xs py-1 text-center items-center">
-              {error}
-            </p>
-          )}
+          <div className="p-4 relative z-10">
+            {error && (
+              <p className="text-red-500 text-xs py-1 text-center items-center">
+                {error}
+              </p>
+            )}
 
-          <div>
-            <div className="input-form">
-              <label>Thời gian:</label>
-              <input
-                type="datetime-local"
-                name="Thời gian"
-                value={time}
-                onChange={(e) => setTime(e.target.value)}
-              />
-            </div>
-            <div className="input-form">
-              <label>Số lượng:</label>
-              <input type="number" name="Số lượng" placeholder="3.03" />
-            </div>
-            <div className="input-form">
-              <label>Trụ:</label>
-              <select name="Trụ" placeholder="19800" id="">
-                <option value="" disabled selected hidden></option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-              </select>
-            </div>
-            <div className="input-form">
-              <label>Doanh thu:</label>
-              <input type="number" name="Doanh thu" placeholder="60000" id="" />
-            </div>
-            <div className="input-form">
-              <label>Đơn giá:</label>
-              <input type="number" name="Đơn giá" placeholder="19800" id="" />
+            <div>
+              <div className="input-form">
+                <label>Thời gian:</label>
+                <input
+                  type="datetime-local"
+                  name="Thời gian"
+                  value={time}
+                  onChange={(e) => setTime(e.target.value)}
+                />
+              </div>
+              <div className="input-form">
+                <label>Số lượng:</label>
+                <input
+                  type="number"
+                  name="Số lượng"
+                  placeholder="3.03"
+                  value={quantity}
+                  onChange={(e) => setQuantity(e.target.value)}
+                />
+              </div>
+              <div className="input-form">
+                <label>Trụ:</label>
+                <select
+                  name="Trụ"
+                  placeholder="19800"
+                  value={pump}
+                  onChange={(e) => setPump(e.target.value)}
+                >
+                  <option value="" disabled selected hidden></option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                </select>
+              </div>
+              <div className="input-form">
+                <label>Doanh thu:</label>
+                <input
+                  type="number"
+                  name="Doanh thu"
+                  placeholder="60000"
+                  value={revenue}
+                  onChange={(e) => setRevenue(e.target.value)}
+                />
+              </div>
+              <div className="input-form">
+                <label>Đơn giá:</label>
+                <input
+                  type="number"
+                  name="Đơn giá"
+                  placeholder="19800"
+                  value={price}
+                  onChange={(e) => setPrice(e.target.value)}
+                />
+              </div>
             </div>
           </div>
-        </div>
-      </form>
+        </form>
     </div>
   );
 };
